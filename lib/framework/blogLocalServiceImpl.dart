f9af33caebdb61dca48e7dc8e45b25e22f3b4338
zgeniuscoders <zgeniuscoders@gmail.com>
2025-04-09 12:57:49 +0100

    ajout de couleurs et de la validation des champs de saisie dans la page de creation compte

--- lib/framework/blogLocalServiceImpl.dart
+++ lib/framework/blogLocalServiceImpl.dart
@@ -4,10 +4,10 @@ import 'package:blog_mobile/business/services/blogLocalService.dart';
 import 'package:get_storage/get_storage.dart';
 import 'dart:convert';
 
-class BlogLocalNetworkServiceImpl implements BlogLocalService {
+class BlogLocalServiceImpl implements BlogLocalService {
   GetStorage? box;
 
-  BlogLocalNetworkServiceImpl({this.box});
+  BlogLocalServiceImpl({this.box});
 
   @override
   Future<bool> sauvegarderUser(User user) async{
